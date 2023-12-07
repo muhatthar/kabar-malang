1273823c04d8859a4af31a58de69c3d48a5ebdab
MuhAtthar <muhatthar@student.ub.ac.id>
2023-12-08 01:01:38 +0700

    detail layout

---
+++ app/src/main/java/com/example/kabarmalang/detail/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.kabarmalang.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kabarmalang.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
