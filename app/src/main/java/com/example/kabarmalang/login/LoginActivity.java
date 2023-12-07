cfac0898c4ad40085b36bd53e3b6f1d3ccc7d03b
MuhAtthar <muhatthar@student.ub.ac.id>
2023-12-07 13:35:33 +0700

    Update LoginActivity.java

--- app/src/main/java/com/example/kabarmalang/login/LoginActivity.java
+++ app/src/main/java/com/example/kabarmalang/login/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.example.kabarmalang.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.kabarmalang.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText et_email, et_pw;
+    private Button login_btn;
+    private TextView tv_daftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
